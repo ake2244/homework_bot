cd1039a8ebc1258833b0dd6935913a9367af9fe0
Andrey Akimov <87949497+ake2244@users.noreply.github.com>
2025-12-19 21:01:17 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "8445585005:AAG9TRYnvn1Y8iCixd6S4nQZE0pZ7eqJMDM"
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", "489911625"))
+ADMIN_ID = 489911625
