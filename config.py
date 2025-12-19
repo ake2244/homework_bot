3fa0f527c21b4e0d4eff8af1c7ad776b5a266bd7
Andrey Akimov <87949497+ake2244@users.noreply.github.com>
2025-12-19 20:42:42 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+# Получаем переменные из окружения (безопаснее!)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8445585005:AAG9TRYnvn1Y8iCixd6S4nQZE0pZ7eqJMDM")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "489911625"))
