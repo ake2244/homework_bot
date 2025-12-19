18a825e1b1ef25452ffc8b60bf4fa0e04eef53c4
Andrey Akimov <87949497+ake2244@users.noreply.github.com>
2025-12-19 20:55:44 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-# Получаем переменные из окружения (безопаснее!)
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8445585005:AAG9TRYnvn1Y8iCixd6S4nQZE0pZ7eqJMDM")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 ADMIN_ID = int(os.getenv("ADMIN_ID", "489911625"))
